dec8ab67c4c201ce9d0b42c9a52f92337c0d0502
rhallows <r_hallows@yahoo.com>
2016-09-19 16:09:27 -0500

    second

--- main.py
+++ main.py
@@ -15,11 +15,62 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
+import cgi
 
-class MainHandler(webapp2.RequestHandler):
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class blog_posts(db.Model):
+    title = db.StringProperty(required = True)
+    blogPost = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+
+class MainHandler(Handler):
+#    def render_front(self, title="", blogPost="", error=""):
+#        self.render("newpost.html", title=title, blogPost=blogPost, error=error, posts=posts
+        #    posts = db.GqlQuery("SELECT * FROM blog_posts ORDER BY created DESC")
     def get(self):
-        self.response.write('Hello world!')
+        self.render("front.html")
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+
+    def post(self):
+        title = self.request.get("title")
+        blogPost = self.request.get("blogPost")
+
+        if title and blogPost:
+            p = blog_posts(title = title, blogPost = blogPost)
+            p.put()
+
+            self.redirect('/blog')
+        else:
+            error = "We need both a title and a blog post!"
+            self.render("front.html", error = error, title=title, blogPost=blogPost)
+
+class BlogRouteHandler(Handler):
+    def render_blog(self, title="", blogPost=""):
+        posts = db.GqlQuery("SELECT * FROM blog_posts ORDER BY created DESC")
+        self.render("blog.html", title=title, blogPost=blogPost, posts=posts)
+
+    def get(self):
+        self.render_blog()
+
+app = webapp2.WSGIApplication([('/', MainHandler), ('/blog', BlogRouteHandler)], debug=True)
